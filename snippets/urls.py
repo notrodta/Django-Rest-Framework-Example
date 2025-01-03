cc93f273ca12f36fbc867f62d8400e5931cd4ccf
Rod <rody101@gmail.com>
2025-01-03 17:04:05 -0500

    implemented viewsets and routers for more abstractions

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,30 +2,43 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 from django.urls import path, include
+from rest_framework import renderers
+from snippets.views import api_root, SnippetViewSet, UserViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+# The API URLs are now determined automatically by the router.
 
 # API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/',
-        views.SnippetList.as_view(),
-        name='snippet-list'),
-    path('snippets/<int:pk>/',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail')
-])
+urlpatterns = [
+    path('', include(router.urls)),
+]
 
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
 ]
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
